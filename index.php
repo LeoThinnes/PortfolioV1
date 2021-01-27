0f582bd9d5b99bf848fd04effdc122093dafad16
LeoThinnes <63150113+LeoThinnes@users.noreply.github.com>
2021-01-27 17:54:52 +0100

    mise en ligne du projet

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Portfolio Léo THINNES</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<header>
+  <nav class="stroke">
+    <a href="#">Accueil </a>
+    <a href="#redirectionProfil">Mon Profil</a>
+    <a href="#separationProjets">Mes Projets</a>
+    <a href="contact">Me contacter</a>
+  </nav>
+</header>
+<body>
+
+
+<div  class="Accueil">
+  <img src="ressources/images/Moi.jpg" alt="PP">
+  <div class="textAccueil">
+    <h1>Je suis Léo THINNES,</h1>
+    <h2>Bienvenu sur mon Portfolio !</h2>
+  </div>
+</div>
+
+<div id="separationProfil">
+  <img src="ressources/images/ampoules.jpg" alt="code">
+</div>
+<div id="redirectionProfil">
+
+</div>
+<div id="Profil">
+  <h1>Mon profil</h1>
+  <div class="Profil_Text">
+    <p>Actuellement étudiant en IUT informatique à Limoges, je suis quelqu'un qui aime apprendre de nouvelles choses tout le temps.
+     L'informatique est en constante évolution et c'est en parti pour cela que j'ai décidé d'étudier dans ce domaine. Le travail en équipe
+     est quelque chose que j'apprécie beaucoup, mais je suis également capable de mener un projet seul.</p>
+   </div>
+   <h2>Mon Parcours</h2>
+   <div class="etudesExp">
+     <div class="etudes">
+       <h3>Mes Etudes : </h3>
+       <div class="blocEtu">
+         <h4>DUT Informatique</h4>
+         <h5>2019-2021</h5>
+         <p>Institut Universitaire de Technologie du Limousin, 87000 Limoges, France</p>
+         <p>Introduction a l'informatique et à la culture associée.</p>
+       </div>
+       <div class="blocEtu">
+         <h4>Baccalauréat général, spécialité Mathématiques</h4>
+         <h5>2016-2019</h5>
+         <p>Lycée Bernard Palissy, 87400 St Léonard de Noblat, France</p>
+         <p>Introduction au domaine scientifique et à la culture générale.</p>
+       </div>
+    </div>
+       <div class="experience">
+         <h3>Expérience professionnelle : </h3>
+         <div class="blocEtu">
+           <h4>Agent contractuel catégorie C </h4>
+           <h5>juillet-aout 2020</h5>
+           <p>centre national de production des titres, 87280 Limoges, France</p>
+         </div>
+     </div>
+</div>
+<button type="button" formaction="href="ressources/CV_Leo_Thinnes.pdf""><a href="ressources/CV_Leo_Thinnes.pdf" target="_blank">Télécharger mon CV </a></button>
+<div id="separationProjets">
+  <img src="ressources/images/html.jpg" alt="code">
+</div>
+
+<div class="Projets">
+  <h1>Mes Projets</h1>
+
+</div>
+
+</body>
+</html>
